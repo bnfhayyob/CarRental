6941aaba45b3bda8d50b5b85632054bd0362c843
bnfhayyob <boyabenfath@gmail.com>
2025-08-18 15:16:29 +0200

    adding routes to pages and getting done with the navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './components/Navbar'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import Home from './pages/home'
+import Cars from './pages/Cars'
+import CarDetails from './pages/CarDetails'
+import MyBookings from './pages/MyBookings'
 
 const App = () => { 
+
+  const [showLogin ,setShowLogin] = useState(false)
+  const isOwnerPath = useLocation().pathname.startsWith('/owner')
+
   return (
-    <div>
-      <Navbar/>
-    </div>
+    <>
+      {!isOwnerPath && <Navbar setShowLogin={setShowLogin}/>}
+
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/cars' element={<Cars/>} />
+        <Route path='/car-details/:id' element={<CarDetails/>} />
+        <Route path='/my-bookings' element={<MyBookings/>} />
+      </Routes>
+    </>
   )
 }
 
