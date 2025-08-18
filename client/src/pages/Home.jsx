eb3d11cf52a5c1619e22e9456888ba2be95083ea
bnfhayyob <boyabenfath@gmail.com>
2025-08-18 16:08:21 +0200

    adding hero section to home page and start getting hand to featured section

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Hero from '../components/Hero'
 
-const home = () => {
+const Home = () => {
   return (
-    <div>home</div>
+    <>
+      <Hero/>
+    </>
   )
 }
 
-export default home
+export default Home
