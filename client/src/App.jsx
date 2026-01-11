d11c51afd6d7316f8c75af9140cbf0e02b029d4b
bnfhayyob <boyabenfath@gmail.com>
2026-01-11 14:29:29 +0100

    Fix case sensitivity in Home component import for Vercel deployment

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import Navbar from './components/Navbar'
 import { Route, Routes, useLocation } from 'react-router-dom'
-import Home from './pages/home'
+import Home from './pages/Home'
 import Cars from './pages/Cars'
 import CarDetails from './pages/CarDetails'
 import MyBookings from './pages/MyBookings'
